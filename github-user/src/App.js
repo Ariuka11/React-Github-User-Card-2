4c30b2deffaace2716663e22c2798e3f2c910613
Ariunbold <ariun1118@gmail.com>
2019-12-09 21:29:15 -0500

    Added CDU and displayed the name and login

--- github-user/src/App.js
+++ github-user/src/App.js
@@ -5,23 +5,69 @@ import axios from 'axios'
 class App extends React.Component{
 
   state = {
-    input: ''
+    input: '',
+    profile: {},
+    username: 'ariuka11'
   }
 
   componentDidMount(){
-    axios.get(`https://api.github.com/users/ariuka11`)
+    axios.get(`https://api.github.com/users/${this.state.username}`)
       .then(res => {
         console.log("Getting User info:", res)
+        this.setState({
+          ...this.state, 
+          profile: res.data
+        })
       })
       .catch(err => {
         console.log('Not successful:', err)
       })
   }
 
+  componentDidUpdate(prevProps, prevState){
+    if(prevState.username !== this.state.username){
+      axios.get(`https://api.github.com/users/${this.state.username}`)
+      .then(res => {
+        console.log("Getting User info:", res)
+        this.setState({
+          ...this.state, 
+          profile: res.data
+        })
+      })
+      .catch(err => {
+        console.log('Not successful:', err)
+      })
+    }
+  }
+
+  handleChange = e => {
+    this.setState({
+      ...this.state,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.setState({
+      username : this.state.input,
+    })
+  }
+
   render(){
     return (
       <div>
-        Hi
+        {this.state.profile.login}
+        <form onSubmit = {e => this.handleSubmit(e)}>
+          <input 
+            type = 'text'
+            name = 'input'
+            placeholder = 'Search User'
+            value = {this.state.input}
+            onChange = {e => this.handleChange(e)}
+          />
+          <button>Search User</button>
+        </form>
       </div>
     )
   }
